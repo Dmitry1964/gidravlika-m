6e4e88f6be06308e94c94b1f509aa3c1ac379cb4
Dmytry64 <nosdm64@gmail.com>
2023-11-18 09:07:37 +0300

    Исправил title на страницах

--- source/js/modules/breadcrumbs.js
+++ source/js/modules/breadcrumbs.js
@@ -2,6 +2,7 @@ const menuButton = document.querySelector('.breadcrumbs__btn');
 const asideMenu = document.querySelector('.aside-menu');
 const asideMenuList = document.querySelector('.aside-menu__list');
 
+
 const handleMenuButtonClick = () => {
   asideMenu.classList.add('aside-menu--mobile');
 };
@@ -10,6 +11,14 @@ const closeAsideMenu = () => {
   asideMenu.classList.remove('aside-menu--mobile');
 };
 
-menuButton.addEventListener('click', handleMenuButtonClick);
-asideMenu.addEventListener('click', closeAsideMenu);
-asideMenuList.addEventListener('click', (evt) => evt.stopPropagation());
+if (menuButton) {
+  menuButton.addEventListener('click', handleMenuButtonClick);
+}
+
+if (asideMenu) {
+  asideMenu.addEventListener('click', closeAsideMenu);
+}
+
+if (asideMenuList) {
+  asideMenuList.addEventListener('click', (evt) => evt.stopPropagation());
+}
